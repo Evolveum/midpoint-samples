76556b21083a23697bfd0ecc82e18b71fa943144
Viliam Repan <vilo.repan@evolveum.com>
2024-10-18 22:08:33 +0200

    updated setenv script for testing (release)

--- test-setenv.sh
+++ test-setenv.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-export POSTGRES_JDBC_URL=
-export POSTGRES_JDBC_USERNAME=
-export POSTGRES_JDBC_PASSWORD=
+# Variables used by midpoint-samples release/next scripts since test.sh needs existing postgresql database
+# to execute test suite. Database has to have correct schema (tables, sequences, etc.) already present.
+JDBC_URL=
+JDBC_USERNAME=
+JDBC_PASSWORD=
 
+if [ -z "$POSTGRES_JDBC_URL" ]; then
+  export POSTGRES_JDBC_URL=$JDBC_URL
+fi
+
+if [ -z "$POSTGRES_JDBC_USERNAME" ]; then
+  export POSTGRES_JDBC_USERNAME=$JDBC_USERNAME
+fi
+
+if [ -z "$POSTGRES_JDBC_PASSWORD" ]; then
+  export POSTGRES_JDBC_PASSWORD=$JDBC_PASSWORD
+fi
