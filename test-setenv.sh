2f3234c140bd9c26e53535025681c11b39b01b40
Viliam Repan <vilo.repan@evolveum.com>
2025-06-18 09:24:37 +0200

    reverted probably wrong script update (d0d2bc3c)

--- test-setenv.sh
+++ test-setenv.sh
@@ -2,15 +2,9 @@
 
 # Variables used by midpoint-samples release/next scripts since test.sh needs existing postgresql database
 # to execute test suite. Database has to have correct schema (tables, sequences, etc.) already present.
-echo "haha"
-JDBC_URL="jdbc:postgresql://localhost:5432/midtest"
-echo "hihi"
-JDBC_USERNAME=midtest
-JDBC_PASSWORD=password
-POSTGRES_JDBC_URL=""
-POSTGRES_JDBC_USERNAME=""
-POSTGRES_JDBC_PASSWORD=""
-echo $JDBC_URL
+JDBC_URL=
+JDBC_USERNAME=
+JDBC_PASSWORD=
 
 if [ -z "$POSTGRES_JDBC_URL" ]; then
   export POSTGRES_JDBC_URL=$JDBC_URL
